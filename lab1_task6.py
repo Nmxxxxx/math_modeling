c702e2f667a0c80bd1fa1821581e73dd578e7816
Nmxxxxx <65217332+Nmxxxxx@users.noreply.github.com>
2023-10-05 16:06:06 +0000

    trhrth

---
+++ lab1_task6.py
@@ -0,0 +1,14 @@
+a = []
+
+age = int(input('Введи возраст: '))
+a.append(age)
+name = input('Введи имя: ')
+a.append(name)
+login = input('Введи логин: ')
+a.append(login)
+town = input('Введи город: ')
+a.append(town)
+class_number = input('Класс: ')
+a.append(class_number)
+
+print(a)
