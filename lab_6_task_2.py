a98cca692458201912c548e1727e185666ab74d3
Nmxxxxx <65217332+Nmxxxxx@users.noreply.github.com>
2023-12-14 16:45:54 +0000

    123123

--- lab_6_task_2.py
+++ lab_6_task_2.py
@@ -2,18 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 N = 100
 
-def hiperbola(x_min, x_max, N):
-    x = np.linspace(x_min,x_max, N)
+def hiperbola(x_min, x_max,x_min2, x_max2, N):
+    x = np.linspace(x_min, x_max, N)
     k = 10
     y = k / x
 
-
     plt.plot(x, y, color='black')
+    x2 = np.linspace(x_min2, x_max2, N)
+    y = k / x2
+    plt.plot(x2, y, color= 'black')
     # plt.axis('equal') # отключение масштабирования
     plt.savefig('task_2.png')
 
 x_min = -10
-x_max = 10
+x_max = -1
+x_min2 = 1
+x_max2 = 10
 
 if __name__ == '__main__':
-    hiperbola(x_min, x_max , N)
+    hiperbola(x_min, x_max, x_min2, x_max2, N)
