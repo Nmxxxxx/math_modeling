f4da68191dbbff94eace2c6e5f9d10d05ab112c7
Nmxxxxx <65217332+Nmxxxxx@users.noreply.github.com>
2024-01-11 16:34:09 +0000

    asdasdadsasd

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+def interference_of_light_waves(angle_vel, a, time):
+
+    t = a * np.pi / 180 * time
+    r = a * t
+    x = r * np.cos(angle_vel)
+    y = r * np.sin(angle_vel)
+    return x, y
+
+
+
+def animate(i):
+
+    # interference.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.3, time=i))
+    interference2.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.4, time=i))
+    interference3.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.5, time=i))
+    interference4.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.6, time=i))
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+
+    interference, = plt.plot([], [], '-', color='r', label='Interference Of Light waves')
+    interference2, = plt.plot([], [], '-', color='r', label='Interference Of Light waves2')
+    interference3, = plt.plot([], [], '-', color='r', label='Interference Of Light waves3')
+    interference4, = plt.plot([], [], '-', color='r', label='Interference Of Light waves4')
+    edge = 3
+    plt.axis('equal')
+    ax.set_xlim(-edge, edge)
+    ax.set_ylim(-edge, edge)
+
+
+    ani = FuncAnimation(fig, animate, frames=360, interval=30)
+
+    ani.save('Interference_Of_Light_waves.gif', fps=30)
