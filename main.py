834dc1babcab32ef5c624b4453fa4403cf5f59f5
Nmxxxxx <65217332+Nmxxxxx@users.noreply.github.com>
2024-01-27 12:44:20 +0000

    asd

--- main.py
+++ main.py
@@ -3,48 +3,62 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import time
 
-def interference_of_light_waves(angle_vel, a, time):
+def interference_of_light_waves_stable1(x0, r):
+    angle=np.linspace(0, np.pi, 100)
+    x = x0 + r * np.cos(angle)
+    y =  r * np.sin(angle)
 
-    t = a * np.pi / 180 * time
-    r = a * t
-    x = r * np.cos(angle_vel)
-    y = r * np.sin(angle_vel)
+    for j in range(len(y)):
+        if y[j] > 3:
+            y[j] = 3
     return x, y
 
-def interference_of_light_waves_stable(angle_vel, r):
-
-    x = r * np.cos(angle_vel)
-    y = r * np.sin(angle_vel)
+def interference_of_light_waves_stable2(x0, y0, r):
+    angle=np.linspace(0, np.pi, 100)
+    x = x0 + r * np.cos(angle)
+    y = y0 + r * np.sin(angle)
+    return x, y
+def interference_of_light_waves_stable3(x0 ,y0, r):
+    angle=np.linspace(0, np.pi, 100)
+    x = x0 + r * np.cos(angle)
+    y = y0 + r * np.sin(angle)
     return x, y
 
+x, y = [], []
+x2, y2 = [], []
+x3, y3 = [], []
 
+def animate(radius):
+    coords = interference_of_light_waves_stable1(x0=0, r=radius)
+    x.append(coords[0])
+    y.append(coords[1])
+    interference.set_data(x, y)
 
-def animate(i):
+    
+    coords2 = interference_of_light_waves_stable2(x0 = 2.5, y0= 3, r=radius)
+    x2.append(coords2[0])
+    y2.append(coords2[1])
+    interference2.set_data(x2, y2)
 
-    interference.set_data(interference_of_light_waves_stable(angle_vel=np.linspace(0, np.pi, 100), r=0.25))
-    interference2.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.45, time=i))
-    interference3.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.5, time=i))
-    interference4.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.55, time=i))
-    interference5.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.6, time=i))
-    interference6.set_data(interference_of_light_waves(angle_vel=np.linspace(0, np.pi, 100), a=0.65, time=i))
+    coords3 = interference_of_light_waves_stable3(x0 = -2.5, y0 = 3, r=radius)
+    x3.append(coords3[0])
+    y3.append(coords3[1])
+    interference3.set_data(x3, y3)
+
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
 
-    interference, = plt.plot([], [], '-', color='r', label='Interference Of Light waves')
-    interference2, = plt.plot([], [], '-', color='r', label='Interference Of Light waves2')
-    
-    interference3, = plt.plot([], [], '-', color='r', label='Interference Of Light waves3')
-    interference4, = plt.plot([], [], '-', color='r', label='Interference Of Light waves4')
-    interference5, = plt.plot([], [], '-', color='r', label='Interference Of Light waves5')
-    interference6, = plt.plot([], [], '-', color='r', label='Interference Of Light waves6')
-
-    edge = 3
+    interference, = plt.plot([], [], '-', color='black', label='Interference Of Light waves')
+    interference2, = plt.plot([], [], '-', color='black', label='Interference Of Light waves')
+    interference3, = plt.plot([], [], '-', color='black', label='Interference Of Light waves')
+    plt.axhline(y=3, color='purple')
+    edge = 6
     plt.axis('equal')
     ax.set_xlim(-edge, edge)
-    ax.set_ylim(-edge, edge)
+    ax.set_ylim(0, edge)
 
-
-    ani = FuncAnimation(fig, animate, frames=360, interval=30)
+    
+    ani = FuncAnimation(fig, animate, frames=np.linspace(0, 4, 20), interval=30)
 
     ani.save('Interference_Of_Light_waves.gif', fps=30)
