cc8e220afa5678205ad313e51e85601afcaf443f
Nmxxxxx <65217332+Nmxxxxx@users.noreply.github.com>
2024-02-01 16:35:40 +0000

    asd

--- lab_7_task_1.py
+++ lab_7_task_1.py
@@ -4,26 +4,44 @@ from matplotlib.animation import FuncAnimation
 
 def cycloid(R,  time, angle_vel):
 
-    t = angle_vel * np.pi / 180 * time
-    # t = np.arange(-2*np.pi, 2*np.pi, 1000)
+    t = angle_vel  * time
+
     x = R * (t - np.sin(t))
     y = R * (1 - np.cos(t))
     return x, y
-#angle_vel = изменение угла
-def animate(i):
 
-    cycloida.set_data(cycloid(R=0.5,angle_vel=np.linspace(0, 2*np.pi, 1000), time=i))
+def circle_move(R, vx0, time):
+    x0 = vx0 * time 
+    alpha = np.arange(0, 2*np.pi, 0.1)
+    x = x0 + R*np.cos(alpha)
+    y = R + R*np.sin(alpha)
+    return x,y
+
+def dotka(R, angle_vel, time):
+    t = angle_vel  * time
+    x = R * (t - np.sin(t))
+    y = R * (1 - np.cos(t))
+    return x, y
+
+
+
+def animate(i):
+    cycloida.set_data(cycloid(R=0.5,angle_vel=np.linspace(0, 2*np.pi, 100), time=i))
+    dota.set_data(dotka(R = 0.5, angle_vel=1, time = i))
+    circle.set_data(circle_move(R=0.5, vx0=0.5, time=i))
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
 
-    cycloida, = plt.plot([], [], '-', color='purple', label='Циклоида')
+    cycloida, = plt.plot([], [], '-', color='black', label='Циклоида')
+    dota, = plt.plot([], [], 'o', color = 'red')
+    circle, = plt.plot([], [], '-', color='green', label='circle')
 
-    edge = 6
+
     plt.axis('equal')
-    ax.set_xlim(-edge, edge)
-    ax.set_ylim(0, edge)
+    ax.set_xlim(-2, 8)
+    ax.set_ylim(-2, 4)
 
-    ani = FuncAnimation(fig, animate, frames=360, interval=30)
+    ani = FuncAnimation(fig, animate, frames=np.linspace(0, 3*np.pi, 100), interval=30)
 
     ani.save('cycloida.gif', fps=30)
