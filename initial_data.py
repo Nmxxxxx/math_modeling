345cf7a577ea2dcdc05dd47b52491346163ba800
Nmxxxxx <65217332+Nmxxxxx@users.noreply.github.com>
2024-03-06 18:18:43 +0000

    asdasd

--- initial_data.py
+++ initial_data.py
@@ -32,3 +32,15 @@ v_x0_faeton = 20122 * np.sqrt((1+e)/(1-e))
 print(v_x0_faeton)
 y0_faeton = 0.2 * 149 * 10**9
 v_y0_faeton = 0
+
+
+e0 = 8.85 * (10**12)
+k = 1/(4*np.pi*e0)
+r = 3
+q_large = 1000
+q1 = 10
+q2 = 12
+q3 = 16
+q4 = 123
+q5 = 8
+q6 = 150
