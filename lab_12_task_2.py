5c0a06b98f863c5d7686bcdfd5c9cdd413d66d57
Nmxxxxx <65217332+Nmxxxxx@users.noreply.github.com>
2024-03-09 18:17:43 +0000

    фыв

--- lab_12_task_2.py
+++ lab_12_task_2.py
@@ -3,76 +3,71 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import initial_data_for_2 as si
-import mplcyberpunk
 
 
 plt.style.use('dark_background')
 # Определяем переменую величину
-frames = 720
-seconds_in_year = 365* 24 * 60 * 60
-years = 1
-t = np.linspace(0, 10**(-3), frames)
+frames = 360
+t = np.linspace(0, 10**(-6), frames)
 
 # Определяем функцию для диф.уравнений
 
 def move_func(s, t):
-    (si.x0_earth, si.v_x0_earth, si.y0_earth, si.v_y0_earth,
-      si.x0_merc, si.v_x0_merc, si.y0_merc, si.v_y0_merc,
-      si.x0_venus, si.v_x0_venus, si.y0_venus, si.v_y0_venus,
-      si.x0_mars, si.v_x0_mars, si.y0_mars, si.v_y0_mars,
-      si.x0_faeton, si.v_x0_faeton, si.y0_faeton, si.v_y0_faeton,
-      si.x0_6, si.v_x0_6, si.y0_6, si.v_y0_6) = s
+    (x1, v_x1, y1, v_y1,
+     x2, v_x2, y2, v_y2,
+     x3, v_x3, y3, v_y3,
+     x4, v_x4, y4, v_y4,
+     x5, v_x5, y5, v_y5,
+     x6, v_x6, y6, v_y6) = s
 # -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-    dx_dt_earth = si.v_x0_earth
-    dv_xdt_earth =  (k * si.q_large * si.q1 * si.x0_earth / (si.x0_earth**2 + si.y0_earth**2) **1.5) / m
-    dydt_earth = si.v_y0_earth
-    dy_ydt_earth =  (k *si.q_large * si.q1 * si.y0_earth / (si.x0_earth**2 + si.y0_earth**2) **1.5) / m
+    dx1_dt = v_x1
+    dvx1_dt =  (k * si.q_large * si.q1 * x1 / (x1**2 + y1**2) **1.5 ) / m
+    dy1_dt = v_y1
+    dvy1_dt =  (k * si.q_large * si.q1 * y1 / (x1**2 + y1**2) **1.5) / m
+# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
+    dx2_dt = v_x2
+    dvx2_dt =  (-k * si.q_large * si.q1 * x2 / (x2**2 + y2**2) **1.5 ) / m
+    dy2_dt = v_y2
+    dvy2_dt =  (k * si.q_large * si.q1 * y2 / (x2**2 + y2**2) **1.5) / m
+# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
+    dx3_dt = v_x3
+    dvx3_dt =  (k * si.q_large * si.q1 * x3 / (x3**2 + y3**2) **1.5 ) / m
+    dy3_dt = v_y3
+    dvy3_dt =  (k * si.q_large * si.q1 * y3 / (x3**2 + y3**2) **1.5) / m
+# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
+    dx4_dt = v_x4
+    dvx4_dt =  (-k * si.q_large * si.q1 * x4 / (x4**2 + y4**2) **1.5 ) / m
+    dy4_dt = v_y4
+    dvy4_dt =  (k * si.q_large * si.q1 * y4 / (x4**2 + y4**2) **1.5) / m
+# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
+    dx5_dt = v_x5
+    dvx5_dt =  (-k * si.q_large * si.q1 * x5 / (x5**2 + y5**2) **1.5 ) / m
+    dy5_dt = v_y5
+    dvy5_dt =  (k * si.q_large * si.q1 * y5 / (x5**2 + y5**2) **1.5) / m
+# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
+    dx6_dt = v_x6
+    dvx6_dt =  (k * si.q_large * si.q1 * x6 / (x6**2 + y6**2) **1.5 ) / m
+    dy6_dt = v_y6
+    dvy6_dt =  (k * si.q_large * si.q1 * y6 / (x6**2 + y6**2) **1.5) / m
 
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-    dx_dt_merc = si.v_x0_merc
-    dv_xdt_merc =  (k *si.q_large * si.q2 * si.x0_merc/ (si.x0_merc**2 + si.y0_merc**2) **1.5) / m
-    dydt_merc = si.v_y0_merc
-    dy_ydt_merc =  (k *si.q_large * si.q2 * si.y0_merc / (si.x0_merc**2 + si.y0_merc**2) **1.5) / m
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-    dx_dt_venus = si.v_x0_venus
-    dv_xdt_venus =  (k *si.q_large * si.q3 * si.x0_venus / (si.x0_venus**2 + si.y0_venus**2) **1.5) / m
-    dydt_venus = si.v_y0_venus
-    dy_ydt_venus =  (k *si.q_large * si.q3 * si.y0_venus / (si.x0_venus**2 + si.y0_venus**2) **1.5) / m
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-    dx_dt_mars = si.v_x0_mars
-    dv_xdt_mars = (- k *si.q_large * si.q4 * si.x0_mars / (si.x0_mars**2 + si.y0_mars**2) **1.5) / m
-    dydt_mars = si.v_y0_mars
-    dy_ydt_mars = (- k *si.q_large * si.q4 * si.y0_mars / (si.x0_mars**2 + si.y0_mars**2) **1.5) / m
-
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-    dx_dt_faeton = si.v_x0_faeton
-    dv_xdt_faeton = (- k *si.q_large * si.q5 * si.x0_faeton / (si.x0_faeton**2 + si.y0_faeton**2) **1.5) / m
-    dydt_faeton = si.v_y0_faeton
-    dy_ydt_faeton = (- k *si.q_large * si.q5 * si.y0_faeton / (si.x0_faeton**2 + si.y0_faeton**2) **1.5) / m
-
-    dx_dt_6 = si.v_x0_6
-    dv_xdt_6 = (- k *si.q_large * si.q5 * si.x0_6  / (si.x0_6**2 + si.y0_6**2) **1.5) / m
-    dydt_6 = si.v_y0_6
-    dy_ydt_6 = (- k *si.q_large * si.q5 * si.y0_6  /  (si.x0_6**2 + si.y0_6**2) **1.5) / m
-
-    return (dx_dt_earth,  dv_xdt_earth, dydt_earth, dy_ydt_earth,
-            dx_dt_merc,   dv_xdt_merc,  dydt_merc, dy_ydt_merc,
-            dx_dt_venus,  dv_xdt_venus, dydt_venus, dy_ydt_venus,
-            dx_dt_mars,   dv_xdt_mars,  dydt_mars, dy_ydt_mars,
-            dx_dt_faeton, dv_xdt_faeton, dydt_faeton, dy_ydt_faeton,
-            dx_dt_6, dv_xdt_6, dydt_6, dy_ydt_6)
+    return (dx1_dt, dvx1_dt, dy1_dt, dvy1_dt, 
+            dx2_dt, dvx2_dt, dy2_dt, dvy2_dt,
+            dx3_dt, dvx3_dt, dy3_dt, dvy3_dt,
+            dx4_dt, dvx4_dt, dy4_dt, dvy4_dt,
+            dx5_dt, dvx5_dt, dy5_dt, dvy5_dt,
+            dx6_dt, dvx6_dt, dy6_dt, dvy6_dt,)
 
 k = 9 * 10**(9)
-m = 1.98 * 10**(30)
+m = 9.1093837 * 10**(-31)
 
-s0 = (si.x0_earth, si.v_x0_earth, si.y0_earth, si.v_y0_earth,
-      si.x0_merc, si.v_x0_merc, si.y0_merc, si.v_y0_merc,
-      si.x0_venus, si.v_x0_venus, si.y0_venus, si.v_y0_venus,
-      si.x0_mars, si.v_x0_mars, si.y0_mars, si.v_y0_mars,
-      si.x0_faeton, si.v_x0_faeton, si.y0_faeton, si.v_y0_faeton,
-      si.x0_6, si.v_x0_6, si.y0_6, si.v_y0_6)
+s0 = (si.x10, si.v_x10, si.y10, si.v_y10,
+      si.x20, si.v_x20, si.y20, si.v_y20,
+      si.x30, si.v_x30, si.y30, si.v_y30,
+      si.x40, si.v_x40, si.y40, si.v_y40,
+      si.x50, si.v_x50, si.y50, si.v_y50,
+      si.x60, si.v_x60, si.y60, si.v_y60)
 sol = odeint(move_func, s0, t)
-
+print(sol)
 # Решение диф. Уравнения
 def solve_func(i, key):
     if key == 'point':
@@ -94,23 +89,24 @@ def solve_func(i, key):
         x6 = sol[i, 20]
         y6 = sol[i, 22]
     else:
-        x1 = sol[i, 0]
-        y1 = sol[i, 2]
+        x1 = sol[:i, 0]
+        y1 = sol[:i, 2]
 
-        x2 = sol[i, 4]
-        y2 = sol[i, 6]
+        x2 = sol[:i, 4]
+        y2 = sol[:i, 6]
 
-        x3 = sol[i, 8]
-        y3 = sol[i, 10]
+        x3 = sol[:i, 8]
+        y3 = sol[:i, 10]
 
-        x4 = sol[i, 12]
-        y4 = sol[i, 14]
+        x4 = sol[:i, 12]
+        y4 = sol[:i, 14]
 
-        x5 = sol[i, 16]
-        y5 = sol[i, 18]
+        x5 = sol[:i, 16]
+        y5 = sol[:i, 18]
 
-        x6 = sol[i, 20]
-        y6 = sol[i, 22]
+        x6 = sol[:i, 20]
+        y6 = sol[:i, 22]
+        
     return ((x1, y1), (x2, y2), (x3, y3), (x4, y4), (x5, y5), (x6, y6))
 
 # Строим график
@@ -118,25 +114,23 @@ def solve_func(i, key):
 fig, ax = plt.subplots()
 # -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
 ball1, = plt.plot([], [], 'o', color='b')
-ball_line1, = plt.plot([], [], '-', color = 'b')
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
+ball_line1, = plt.plot([], [], '-', color='b')
 
 ball2, = plt.plot([], [], 'o', color='r')
-ball_line2, = plt.plot([], [], '-', color = 'r')
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-ball3, = plt.plot([], [], 'o', color='white')
-ball_line3, = plt.plot([], [], '-', color = 'white')
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-ball4, = plt.plot([], [], 'o', color='r')
-ball_line4, = plt.plot([], [], '-', color = 'r')
-# -=---==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-==-==-=-=-=-=-=-=-=-=-=
-ball5, = plt.plot([], [], 'o', color='grey')
-ball_line5, = plt.plot([], [], '-', color = 'grey')
+ball_line2, = plt.plot([], [], '-', color='r')
 
-ball6, = plt.plot([], [], 'o', color='g')
-ball_line6, = plt.plot([], [], '-', color = 'g')
+ball3, = plt.plot([], [], 'o', color='purple')
+ball_line3, = plt.plot([], [], '-', color='purple')
 
-plt.plot([0], [0], 'o', color='orange', ms=10)
+ball4, = plt.plot([], [], 'o', color='green')
+ball_line4, = plt.plot([], [], '-', color='green')
+
+ball5, = plt.plot([], [], 'o', color='white')
+ball_line5, = plt.plot([], [], '-', color='white')
+
+ball6, = plt.plot([], [], 'o', color='brown')
+ball_line6, = plt.plot([], [], '-', color='brown')
+plt.plot([0], [0], 'o', color='orange', ms=15)
 
 def animate(i):
     ball1.set_data(solve_func(i, 'point')[0])
@@ -144,7 +138,7 @@ def animate(i):
 
     ball2.set_data(solve_func(i, 'point')[1])
     ball_line2.set_data(solve_func(i, 'line')[1])
-    
+
     ball3.set_data(solve_func(i, 'point')[2])
     ball_line3.set_data(solve_func(i, 'line')[2])
 
@@ -154,7 +148,6 @@ def animate(i):
     ball5.set_data(solve_func(i, 'point')[4])
     ball_line5.set_data(solve_func(i, 'line')[4])
 
-    
     ball6.set_data(solve_func(i, 'point')[5])
     ball_line6.set_data(solve_func(i, 'line')[5])
 
@@ -164,4 +157,4 @@ plt.axis('equal')
 edge = 0.5
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
-ani.save('lab_12_task_2.gif')
+ani.save('lab_12_task_dopad_2.gif')
